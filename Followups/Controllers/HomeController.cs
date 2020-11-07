45613ebca82ed61999d72b14371bfc91c0b23edc
Moti Augustine <motipa@gmail.com>
2020-11-07 13:44:38 +0400

    initial commit

---
+++ Followups/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Followups.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace Followups.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        const string SessionName = "_Name";
+        const string SessionAge = "_Age";
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+
+        {
+            
+            ViewBag.Name = HttpContext.Session.GetString(SessionName);
+            ViewBag.Title = "Admin";
+            return View();
+        }
+        public IActionResult User()
+        {
+            ViewBag.Name = HttpContext.Session.GetString(SessionName);
+            ViewBag.Title = "User";
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+      
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Followups/Models/DB/FollowUpDbContext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Followups.Models.DB
+{
+    public partial class FollowUpDbContext : DbContext
+    {
+        public FollowUpDbContext()
+        {
+        }
+
+        public FollowUpDbContext(DbContextOptions<FollowUpDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<User> User { get; set; }
+
+     
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.Property(e => e.CreatedDate)
+                    .HasColumnName("created_date")
+                    .HasColumnType("datetime");
+
+                entity.Property(e => e.ModifiedDate)
+                    .HasColumnName("modified_date")
+                    .HasColumnType("datetime");
+
+                entity.Property(e => e.Password)
+                    .HasColumnName("password")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Type)
+                    .HasColumnName("type")
+                    .HasMaxLength(10)
+                    .IsFixedLength();
+
+                entity.Property(e => e.Username)
+                    .HasColumnName("username")
+                    .HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
